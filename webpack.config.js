1a94ee6ff20682ffb99479db2838757451bfd89a
erwan <erwanp.vallee@gmail.com>
2020-02-19 11:38:51 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html")
+    })
+  ],
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    inline: true,
+    open: true,
+    hot: true,
+    port: 4000
+  }
+};
