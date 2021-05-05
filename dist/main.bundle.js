473573d7fe3fe864ba64129f799372fbc85c6340
dymafr <contact@dyma.fr>
2021-05-05 15:20:29 +0200

    maj

---
+++ dist/main.bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var test = 42;
+console.log(test);
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
